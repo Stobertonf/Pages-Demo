10c5fb19257cf9b1f6ff95f6c2cbe54300da9b14
stobertonf <stobertonf@gmail.com>
2022-09-23 13:41:43 -0300

    first commit

---
+++ Pages-Demo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Pages_Demo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Pages-Demo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Pages_Demo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
